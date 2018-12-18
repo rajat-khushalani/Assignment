e370e25a050440aa54a53f5c3a67cb686111e1f4
rajat-khushalani <45480546+rajat-khushalani@users.noreply.github.com>
2018-12-18 11:13:44 +0530

    Updated CalculatorMenu code

--- Calculator/src/CalculatorMenu.java
+++ Calculator/src/CalculatorMenu.java
@@ -2,12 +2,26 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculatorMenu {
+
+
+        public static int userChoice(){
+            System.out.println("Please Select Operation to Perform:");
+            System.out.println("1-Addition");
+            System.out.println("2-Subtraction");
+            System.out.println("3-Division");
+            System.out.println("4-Multiplication");
+            System.out.println("5-Exit");
+            Scanner sc=new Scanner(System.in);
+            return sc.nextInt();
+        }
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         double num1 = 0;
         double num2 = 0;
-        int userChoice=0;
+        int userChoice=userChoice();
+
         while (userChoice!=5) {
             NumberCalculator calc = new NumberCalculator();
             try {
@@ -18,27 +32,27 @@ public class CalculatorMenu {
             } catch (InputMismatchException e) {
                 System.out.println("Invalid Input Type");
             }
-            System.out.println("Please Select Operation to Perform:");
-            System.out.println("1-Addition");
-            System.out.println("2-Subtraction");
-            System.out.println("3-Division");
-            System.out.println("4-Multiplication");
-            userChoice = input.nextInt();
+
+
 
             switch (userChoice) {
                 case 1:
-                    System.out.println(calc.add(num1, num2));
+                    System.out.println("Addition"+calc.add(num1, num2));
                     break;
                 case 2:
-                    System.out.println(calc.subtract(num1, num2));
+                    System.out.println("Substraction"+calc.subtract(num1, num2));
                     break;
                 case 3:
-                    System.out.println(calc.divide(num1, num2));
+                    System.out.println("Division"+calc.divide(num1, num2));
                     break;
                 case 4:
-                    System.out.println(calc.multiply(num1, num2));
+                    System.out.println("Multipication"+calc.multiply(num1, num2));
+                    break;
+                case 5:
+                    System.out.println("Thank YOU");
                     break;
             }
+            userChoice=userChoice();
 
         }
 
