076f9cab40cc8ad5376d300fa98525ddac2a8e03
rajat-khushalani <45480546+rajat-khushalani@users.noreply.github.com>
2018-12-18 10:44:16 +0530

    NavigationDrawer

---
+++ NavigationDrawer/src/main/java/navigationdeawerviewpager/gl/com/navigationdeawerviewpager/HomeActivity.java
@@ -0,0 +1,108 @@
+package navigationdeawerviewpager.gl.com.navigationdeawerviewpager;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private NavigationView nvDrawer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        nvDrawer = findViewById(R.id.nav_view);
+
+                // ...From section above...
+                // Find our drawer view
+
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment=null;
+        Class fragmentClass=PatientInfoFragment.class;
+        switch(menuItem.getItemId()) {
+            case R.id.patient_information:
+                fragmentClass = PatientInfoFragment.class;
+                break;
+            case R.id.about:
+                fragmentClass = AboutFragment.class;
+                break;
+
+                //fragmentClass = AboutFragment.class;
+
+            case R.id.programming:
+                fragmentClass = ProgrammingFragment.class;
+                break;
+            default:
+                fragmentClass = PatientInfoFragment.class;
+                break;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawerLayout.closeDrawers();
+    }
+
+
+}
